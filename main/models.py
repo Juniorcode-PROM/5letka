2244222530ccde10fe6b6e4d6a5387e3d3362dc6
Saddam-Husein-Oreshnik <ponalprinal595@gmail.com>
2025-05-04 19:16:21 +0500

    Add model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import ForeignKey
+
+
 from django.db import models
 
 # Create your models here.
+
+
+class task(models.Model):
+    class Status(models.TextChoices):
+        NOT_STARTED = "N", "Not started"
+        IN_PROGRESS = "I", "In progress"
+        DONE = "D", "Done"
+
+    title = models.CharField(max_length=110)
+    text = models.TextField()
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=Status, default=Status.NOT_STARTED)
+    deadline = models.DateTimeField(null=True, default=None)
