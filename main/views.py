7608b43eb9531d3c218e0c396f0f591426d16393
Saddam-Husein-Oreshnik <ponalprinal595@gmail.com>
2025-05-11 19:20:37 +0500

    Fix ruff

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 
 from main.forms import RegistrationForm
 from main.models import Task
@@ -19,6 +19,7 @@ def registration_view(request):
     return render(request, "register.html", {"form": form})
 
 def viewing_task(request, task_id):
+    """ъэ функция в 2 долбанных строчки, делающая очень много."""
     task = get_object_or_404(Task, id=task_id)
     # TODO: fill template name
     return render(request, ".html", {"task": task})
