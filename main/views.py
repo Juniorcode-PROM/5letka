ce6fbb64fc31e4bf93fa2fb763f18d084dd48a86
Saddam-Husein-Oreshnik <ponalprinal595@gmail.com>
2025-05-11 18:26:52 +0500

    Add controller viewing_task

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from main.forms import RegistrationForm
+from main.models import Task
 
 
 def registration_view(request):
@@ -16,3 +17,8 @@ def registration_view(request):
     else:
         form = RegistrationForm()
     return render(request, "register.html", {"form": form})
+
+def viewing_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    # TODO: fill template name
+    return render(request, ".html", {"task": task})
