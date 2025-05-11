8af252fcbe5a579bff7947a8d735356eef0e3895
Saddam-Husein-Oreshnik <ponalprinal595@gmail.com>
2025-05-11 19:47:25 +0500

    snapshot

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
-from django.shortcuts import redirect, render
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect, render, get_object_or_404
 
 from main.forms import RegistrationForm
+from main.models import Task
 
 
 def registration_view(request):
@@ -16,3 +18,11 @@ def registration_view(request):
     else:
         form = RegistrationForm()
     return render(request, "register.html", {"form": form})
+
+
+def edit_task_view(request, task_id):
+    """функция для изменения задачи пользователем."""
+    task = get_object_or_404(Task, id=task_id)
+    if task.author != request.user:
+        raise PermissionDenied
+    if request.metho
