507e61e1db892e945aebd9800fd0f3c91d0230fc
Saddam-Husein-Oreshnik <ponalprinal595@gmail.com>
2025-05-11 18:52:21 +0500

    Add controller delete_task

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from main.forms import RegistrationForm
+from main.models import Task
 
 
 def registration_view(request):
@@ -16,3 +17,9 @@ def registration_view(request):
     else:
         form = RegistrationForm()
     return render(request, "register.html", {"form": form})
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect("/")
